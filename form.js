7f248a529e85862a8f5852784ca3d4bf4cdffb23
Mohan <85388246+MohanRaj-Dev@users.noreply.github.com>
2023-09-19 21:54:04 +0530

    Update form.js

--- form.js
+++ form.js
@@ -1,17 +1,18 @@
 var form  = document.querySelector('form');
+var user = prompt('Enter Your User Name: ');
+
 form.addEventListener("submit", (e)=> {
     e.preventDefault();
-
     var msg = document.getElementById('myInput').value
-
+    var result = `${user}: ${msg}`
     var token = "6089962515:AAF_My6RRQGuJcsHfJ1u-2FXwKdBESIPcVw"
     var chat_id = 6194528557
 
-    var url = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chat_id}&text=${msg}`
+    var url = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chat_id}&text=${result}`
 
     api = new XMLHttpRequest();
     api.open("GET", url, true);
     api.send();
-    console.log("Message send Successfully!!")
-    alert("Message Sent Successfully !! check here https://t.me/red_msg_bot")
+    console.log(result)
+    alert(`Message Successfully Sent By ${user}`)
 })
